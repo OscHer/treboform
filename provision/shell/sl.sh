d9f523dce9634584446ff87f66f8d6ef5a4106d1
oscar_heredia <9662727-OscHer@users.noreply.gitlab.com>
2023-03-12 17:49:08 +0100

    C Esqueleto de secondary logging funcional
    El formato es feo. Da dos líneas en lugar de una y no sigue un estándar
    medianamente humano; pero bueno: es funcional.

--- provision/shell/sl.sh
+++ provision/shell/sl.sh
@@ -13,4 +13,18 @@ SL_SCRIPT="/etc/profile.d/sl.sh" # Script que importaremos al iniciar una termin
 # TODO: considerar ponerlo en .bashrc para cubrir también sesiones sin login. 
 
 
-[[ ! -d $SL_DIRECTORY ]] && mkdir -p $SL_DIRECTORY 2> /dev/null
+# TODO: introducir todo esto en una función
+# TODO: una linea por entrada, eliminar esos comentarios
+# TODO: estandarizar el formato para rsyslog
+# TODO: integrar con journald
+# TODO-chavo: YA CÁLLATE, CÁLLATE, CÁLLATE!!! QUE ME DESESPERAS!!
+_who_am_i=$(who am i|awk '{print $1}')
+_ID=$(id -u $_who_am_i)
+
+if [ "$_ID" > 0 ]
+then
+export HISTSIZE=10000
+export HISTTIMEFORMAT='%F %T '
+export HISTFILE=$SL_DIRECTORY/sl-$(who am i | awk '{print $1}';exit)-$(date +%F)
+export PROMPT_COMMAND='history -a'
+fi
--- provision/shell/sl_install.sh
+++ provision/shell/sl_install.sh
@@ -4,7 +4,13 @@
 
 ORIGIN="/tmp/sl.sh"
 DESTINATION="/etc/profile.d/sl.sh" # De esta forma es más discreto
+SL_DIRECTORY="/var/log/sl/"
 
 mv -f $ORIGIN $DESTINATION && echo "SL movido con éxito"
 chown root:root $DESTINATION && echo "SL instalado con éxito"
 # TODO: eliminar archivo en directorio temporal $ORIGIN
+
+# Creamos directorio de logs y añadimos permisos adicionales
+[[ ! -d $SL_DIRECTORY ]] && mkdir -p $SL_DIRECTORY 2> /dev/null
+chmod o+w $SL_DIRECTORY
+chmod +t $SL_DIRECTORY # Queremos el sticky bit para evitar borrados  # TODO: considerar generar directorios por usuario y apligar SGID
