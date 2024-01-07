05721b2f568e54fdebddf281011b9d710e0ec580
Oriol <oriol.fersan@gmail.com>
2024-01-07 20:18:46 +0100

    Docs: added a working IP variable command

--- provision/files/secondary/sl.sh
+++ provision/files/secondary/sl.sh
@@ -1,14 +1,10 @@
 # Secondary Logging 
 
-export HISTSIZE=5000
-FROM= $(who am i | awk '{print $1}')
-AS= $(whoami)
-TERMINAL= $(who am i | awk '{print $2}')
-IP= $(who am i | awk '{print $5}')
+FROM= $(who am i | awk '{print $1}') #This variable will show the name of the user that has written the command.
+AS= $(whoami) #This variable will show the name of the effective user that the user is operating as.
+TERMINAL= $(who am i | awk '{print $2}') #This variable will show the terminal where the user is operating.
+IP= $(who --ips | grep $TERMINAL | awk '{print $5}') #This variable will show the IP of the user.
 
-[ -d /var/log/history/SecondaryLogging ] || mkdir -p /var/log/secondarylogging
-chmod 700 /var/log/secondarylogging
-
-export PROMPT_COMMAND= echo "$(date "+%Y-%m-%d.%H:%M:%S") $FROM $AS $TERMINAL $IP $(pwd) $(history 1)" >> /var/log/secondarylogging/$(date "+%Y-%m-%d")
+export PROMPT_COMMAND= 'echo "$(date "+%Y-%m-%d.%H:%M:%S") $FROM $AS $TERMINAL $IP $(pwd) $(history 1)" >> /var/log/secondarylogging/$(date "+%Y-%m-%d")'
 
 
