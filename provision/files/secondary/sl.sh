df18950de79e94788062dda88e291971552f3298
Oriol <oriol.fersan@gmail.com>
2024-01-17 18:51:35 +0100

    Fix: now sl.sh works with a logger

--- provision/files/secondary/sl.sh
+++ provision/files/secondary/sl.sh
@@ -1,10 +1,10 @@
 # Secondary Logging 
 
-FROM= $(who am i | awk '{print $1}') #This variable will show the name of the user that has written the command.
-AS= $(whoami) #This variable will show the name of the effective user that the user is operating as.
-TERMINAL= $(who am i | awk '{print $2}') #This variable will show the terminal where the user is operating.
-IP= $(who --ips | grep $TERMINAL | awk '{print $5}') #This variable will show the IP of the user.
+FROM=$(who am i | awk '{print $1}') #This variable will show the name of the user that has written the command.
+AS=$(whoami) #This variable will show the name of the effective user that the user is operating as.
+TERMINAL=$(who am i | awk '{print $2}') #This variable will show the terminal where the user is operating.
+IP=$(who --ips | grep $TERMINAL | awk '{print $5}') #This variable will show the IP of the user.
 
-export PROMPT_COMMAND= 'echo "$(date "+%Y-%m-%d.%H:%M:%S") $FROM $AS $TERMINAL $IP $(pwd) $(history 1)" >> /var/log/secondarylogging/$(date "+%Y-%m-%d")'
+export PROMPT_COMMAND='logger --server XX -p local0.notice --tcp "$(date "+%Y-%m-%d.%H:%M:%S") $FROM $AS $TERMINAL $IP $(pwd) $(history 1)' 
 
 
