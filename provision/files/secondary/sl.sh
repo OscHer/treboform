af7d998ce0c663b97a2adb0a7abf37e76403d451
Oriol <oriol.fersan@gmail.com>
2024-01-19 15:43:28 +0100

    fix command

--- provision/files/secondary/sl.sh
+++ provision/files/secondary/sl.sh
@@ -5,6 +5,6 @@ AS=$(whoami) #This variable will show the name of the effective user that the us
 TERMINAL=$(who am i | awk '{print $2}') #This variable will show the terminal where the user is operating.
 IP=$(who --ips | grep $TERMINAL | awk '{print $5}') #This variable will show the IP of the user.
 
-export PROMPT_COMMAND='logger --server XX -p local0.notice --tcp "$(date "+%Y-%m-%d.%H:%M:%S") $FROM $AS $TERMINAL $IP $(pwd) $(history 1)' 
+export PROMPT_COMMAND='logger -n XX -p local0.notice --tcp $(date "+%Y-%m-%d.%H:%M:%S") $FROM $AS $TERMINAL $IP $(pwd) $(history 1)' 
 
 
