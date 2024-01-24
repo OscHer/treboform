b25cb95b17c05e9a2c70d7ee6bba254bd3f6d9ed
Oriol <oriol.fersan@gmail.com>
2024-01-24 14:07:00 +0100

    Fix: history number now is omited

--- provision/files/secondary/sl.sh
+++ provision/files/secondary/sl.sh
@@ -5,9 +5,10 @@ AS=$(whoami) #This variable will show the name of the effective user that the us
 TERMINAL=$(who am i | awk '{print $2}') #This variable will show the terminal where the user is operating.
 IP=$(who am i | awk '{print $5}') #This variable will show the IP of the user.
 ## THIS OPTION IS NOW DEPRECATED: IP=$(who --ips | grep $TERMINAL | awk '{print $5}') #This variable will show the IP of the user.
+HISTORY=$(history 1 | tr -s " " | cut -f 3- -d " " -s)
 
 
-export PROMPT_COMMAND='logger -p local0.notice $(date "+%Y-%m-%d.%H:%M:%S") $FROM $AS $TERMINAL $IP $(pwd) $(history 1)' 
+export PROMPT_COMMAND='logger -p local0.notice $(date "+%Y-%m-%d.%H:%M:%S") $FROM $AS $TERMINAL $IP $(pwd) $HISTORY' 
 ##export PROMPT_COMMAND='logger -n XX -P 514 -p local0.notice --tcp $(date "+%Y-%m-%d.%H:%M:%S") $FROM $AS $TERMINAL $IP $(pwd) $(history 1)'
 
 
