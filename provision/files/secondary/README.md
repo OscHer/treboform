91ae02fc349453800b75341ef7fbb193982451bf
Oriol <oriol.fersan@gmail.com>
2024-01-25 13:44:22 +0100

    Docs: remove outputman and update README

--- provision/files/secondary/README.md
+++ provision/files/secondary/README.md
@@ -1,3 +1,5 @@
 # Secondary Logging
 
+# Secondary Logging Output Fields Descriptor
 
+User logged - Effective user - Terminal - IP - Working directory - Command
--- provision/files/secondary/sl.sh
+++ provision/files/secondary/sl.sh
@@ -4,11 +4,12 @@ FROM=$(who am i | awk '{print $1}') #This variable will show the name of the use
 AS=$(whoami) #This variable will show the name of the effective user that the user is operating as.
 TERMINAL=$(who am i | awk '{print $2}') #This variable will show the terminal where the user is operating.
 IP=$(who am i | awk '{print $5}') #This variable will show the IP of the user.
-## THIS OPTION IS NOW DEPRECATED: IP=$(who --ips | grep $TERMINAL | awk '{print $5}') #This variable will show the IP of the user.
-HISTORY=$(history 1 | tr -s " " | cut -f 3- -d " " -s)
+## THIS OPTION IS NOW DEPRECATED: IP=$(who --ips | grep $TERMINAL | awk '{print $5}') 
+
+#HISTORY=$(history 1 | tr -s " " | cut -f 3- -d " " -s)
 
 
-export PROMPT_COMMAND='logger -p local0.notice $FROM $AS $TERMINAL $IP $(pwd) $HISTORY' 
+export PROMPT_COMMAND='logger -p local0.notice $FROM $AS $TERMINAL $IP $(pwd) $(history 1 | tr -s " " | cut -f 3- -d " " -s)'
 ##export PROMPT_COMMAND='logger -n XX -P 514 -p local0.notice --tcp $(date "+%Y-%m-%d.%H:%M:%S") $FROM $AS $TERMINAL $IP $(pwd) $(history 1)'
 
 
