a45cd4861c242c218a52099da93f6811058c1b42
Oscar <oscarherediallorens@gmail.com>
2024-08-29 17:07:58 +0200

    Added imalive script skeleton

---
+++ provision/shell/imalive.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+######################################################################
+# @author      : Óscar Heredia (65867332+OscHer@users.noreply.github.com)
+# @file        : imalive.sh
+# @created     : martes ago 27, 2024 08:15:58 CEST
+#
+# @description : Executed only to test synced folder from every node and
+#                feed it with guest data 
+######################################################################
+
+hostname >> /vagrant/ourdata
+
