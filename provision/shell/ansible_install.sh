67ff656f363c041465f7efe71f380b4cd4924fab
Oscar <oscarherediallorens@gmail.com>
2024-09-15 17:38:48 +0200

    Add ansible installation as a shell provisioner

---
+++ provision/shell/ansible_install.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+######################################################################
+# @author      : Óscar Heredia (65867332+OscHer@users.noreply.github.com)
+# @file        : ansible_install.sh
+# @created     : domingo sep 15, 2024 10:09:53 CEST
+#
+# @description : Install ansible from official repositories 
+######################################################################
+
+apt-get install ansible -y
+
+
+
